153f341e9b64b0895b53e13791703524016be76b
ellie turnock <ebturnock@gmail.com>
2019-01-24 16:30:41 +0000

    refactored feature tests, added sign in method

--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -1,17 +1,11 @@
 feature 'player names' do
   scenario 'players should be able to enter names' do
-    visit ('/')
-    fill_in :player1, with: 'Ellie'
-    fill_in :player2, with: 'James'
-    click_button 'Submit'
+    include sign_in_and_play
     expect(page).to have_content 'Ellie vs James - it\'s on.'
   end
 
   scenario 'player1 can view player2 HP' do
-    visit ('/')
-    fill_in :player1, with: 'Ellie'
-    fill_in :player2, with: 'James'
-    click_button 'Submit'
+    include sign_in_and_play
     expect(page).to have_content 'James: 100HP'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,7 @@ ENV['RACK_ENV'] = 'test'
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
-
+require 'features/web_helpers'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 Capybara.app = Battle
