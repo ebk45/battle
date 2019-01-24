ac0b9c860a46671a47f8b9bcc9b5cf84f38dfab7
ellie turnock <ebturnock@gmail.com>
2019-01-24 15:58:21 +0000

    test for viewing player names and HP

--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -6,4 +6,12 @@ feature 'player names' do
     click_button 'Submit'
     expect(page).to have_content 'Ellie vs James - it\'s on.'
   end
+
+  scenario 'player1 can view player2 HP' do
+    visit ('/')
+    fill_in :player1, with: 'Ellie'
+    fill_in :player2, with: 'James'
+    click_button 'Submit'
+    expect(page).to have_content 'James: 100HP'
+  end
 end
